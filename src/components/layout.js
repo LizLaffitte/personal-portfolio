a79233ddc24c7584fd3ec4286014d4f9ab78aa88
lizlaffitte <laffitte.digital@gmail.com>
2021-04-11 20:50:35 -0400

    Set config options for Typography.js and use in layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
 import React from "react"
 import {Link} from "gatsby"
 
+const ListLink = props => (
+    <li style={{display:`inline-block`, marginRight:`1rem`}}>
+        <Link to={PushSubscriptionOptions.to}>{props.children}</Link>
+    </li>
+)
+
 export default function Layout({children}){
     return(
         <div style={{margin: `1rem auto`, maxWidth: 900, padding: `0 1rem`}}>
-            <header style={{marginBottom: `2rem`}}>
-                <Link to="/"><h3 style={{display:`inline`}}>Liz Laffitte</h3></Link>
+            <header style={{marginBottom: `2rem`, textAlign:`center`}}>
+                <img src="" />
+                <h3>Liz Laffitte</h3>
             </header>
             {children}
         </div>

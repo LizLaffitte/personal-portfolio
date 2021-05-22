ca843ddf354563e44e4b7f744504010cffe79dee
lizlaffitte <laffitte.digital@gmail.com>
2021-05-22 15:22:48 -0400

    Moved all inline styles on index page to internal stylesheet.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home({data}) {
       <h2>About</h2>
       
       <div className="flx">
-      <StaticImage src="../images/Liz-Laffitte.png"  objectPosition="50% top" alt="Liz Laffitte Headshot" style={{minWidth:"200px", maxWidth:'400px'}} />  
+      <StaticImage src="../images/Liz-Laffitte.png"  objectPosition="50% top" alt="Liz Laffitte Headshot" id="headshot" />  
           <div className="txt">
             <p>I'm passionate about helping people, building user-centric software and learning new things.</p>
             <p>I was first introduced to programming when I was tasked with building HTML emails that look good in all versions of Outlook. I was hooked. Several programming languages, frameworks and one software engineering bootcamp later, I am looking for my first full-time software engineering position.</p>
@@ -32,9 +32,9 @@ export default function Home({data}) {
     </InnerSection>
     <InnerSection sectionId="contact" >
     <h2>Contact</h2>
-    <p style={{textAlign:`center`}}>Questions? Comments? Excellent puns? Connect with me online or send me a message via the form.</p>
+    <p>Questions? Comments? Excellent puns? Connect with me online or send me a message via the form.</p>
       <div >
-        <p style={{textAlign:"center"}}>Find me on <a href="https://www.linkedin.com/in/lizlaffitte/" target="_blank" rel="noreferrer">LinkedIn</a>, <a href="https://twitter.com/MrsSquishyface" target="_blank" rel="noreferrer">Twitter</a> and <a href="https://github.com/lizlaffitte" target="_blank" rel="noreferrer">GitHub</a>.</p>
+        <p>Find me on <a href="https://www.linkedin.com/in/lizlaffitte/" target="_blank" rel="noreferrer">LinkedIn</a>, <a href="https://twitter.com/MrsSquishyface" target="_blank" rel="noreferrer">Twitter</a> and <a href="https://github.com/lizlaffitte" target="_blank" rel="noreferrer">GitHub</a>.</p>
         <Form />
       </div>
       </InnerSection>
